2a65a0d8e6e1c5adbaafba1bb0a8046dd31f01fd
Matthieu Parent <matthieuparent@outlook.com>
2022-09-08 00:51:17 -0400

    structure de base

---
+++ src/scripts/Main.js
@@ -0,0 +1,12 @@
+class Main {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    console.log(
+      "salut le curieux, n'hésite pas à venir me jaser si tu veux faire du JS avant la mi-session"
+    );
+  }
+}
+new Main();
